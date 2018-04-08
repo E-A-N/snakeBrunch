2675e776198d773fe57f2a1a9e6330f18219b300
e-a-n <ean.director@gmail.com>
2018-04-08 00:02:08 -0700

    converting code to work with architecture

---
+++ conversion/app.js
@@ -0,0 +1,47 @@
+const render = require("./render.js");
+module.exports = function () {
+
+var btn = document.getElementById('btn');
+btn.addEventListener("click", function(){ render.init();});
+
+	document.onkeydown = function(event) {
+		const left = 37;
+		const right = 39;
+		const up = 38;
+		const down = 40;
+
+        keyCode = event.keyCode;
+
+        switch(keyCode) {
+	        case left:
+				if (render.direction != 'right') {
+					render.direction = 'left';
+				}
+				console.log('left');
+			break;
+
+	        case right:
+				if (render.direction != 'left') {
+					render.direction = 'right';
+					console.log('right');
+				}
+			break;
+
+	        case up:
+				if (render.direction != 'down') {
+					render.direction = 'up';
+					console.log('up');
+				}
+			break;
+
+	        case down:
+				if (render.direction != 'up') {
+					render.direction = 'down';
+					console.log('down');
+				}
+			break;
+		}
+      }
+
+
+};
---
+++ conversion/gameConfig.js
@@ -0,0 +1,12 @@
+const config = {};
+config.myCanvas = document.getElementById('mycanvas'); //NOTE: change the canvas id to camelcase
+config.ctx = myCanvas.getContext('2d');
+config.snakeSize = 10;
+config.w = 350;
+config.h = 350;
+config.score = 0;
+config.snake;
+config.snakeSize = 10;
+config.food;
+
+module.exports = config;
